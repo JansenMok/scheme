b30b2d297d86f8ccf80f40ac8d5606fddbc1a817
HarderProblems <jansenmok@berkeley.edu>
2023-11-09 17:58:18 -0800

    Add debugging for Problem 2

--- debug.py
+++ debug.py
@@ -1,2 +1,15 @@
-from scheme_classes import *
+# PROBLEM 2 DEBUGGING
+from scheme import *
+env = create_global_frame()
+twos = Pair(2, Pair(2, nil))
+plus = BuiltinProcedure(scheme_add) # + procedure
+scheme_apply(plus, twos, env) # Type SchemeError if you think this errors
 
+# from scheme_builtins import *
+# print(scheme_add(1))
+# print(scheme_add(2))
+# print(scheme_add(scheme_add(3)))
+# print(scheme_add(1, 3))
+# s= [2, 2, 2]
+# print(scheme_add(2, 2))
+# print(plus.py_func(*s))
