0dcd24e29092eee8904a8c4a5d166df87320e49c
HarderProblems <jansenmok@berkeley.edu>
2023-11-13 18:01:24 -0800

    Debug for Problem 5

--- debug.py
+++ debug.py
@@ -15,8 +15,21 @@ env = create_global_frame()
 # print(plus.py_func(*s))
 
 # PROBLEM 3 DEBUGGING
-from scheme_eval_apply import *
-# expr = Pair(1, Pair(2, Pair(3, nil)))
-expr = Pair('+', Pair(Pair('+', Pair(1, Pair(2, nil))), Pair(69, nil)))
-print(f'original: {expr}')
-scheme_eval(expr, env)
+# from scheme_eval_apply import *
+# # expr = Pair(1, Pair(2, Pair(3, nil)))
+# expr = Pair('+', Pair(Pair('+', Pair(1, Pair(2, nil))), Pair(69, nil)))
+# print(f'original: {expr}')
+# scheme_eval(expr, env)
+
+# PROBLEM 6 DEBUGGING
+from scheme_reader import *
+# from scheme_forms import *
+# do_quote_form(Pair(Pair('+', Pair('x', Pair(2, nil))), nil), env)
+
+# (cons (cons + (cons x (cons 2 nil))) nil)
+
+# print(do_quote_form(read_line("((+ x 2))"), env))
+# print(read_line("((+ x 2))"))
+
+expr = Pair(Pair('+', Pair('x', Pair(2, nil))), nil)
+print(do_quote_form(expr, env))
